da06292e8c4505e53d2d5b9043437ca648b448cc
lirong <lir@atacm.com>
2016-12-22 18:40:05 +0800

    commit

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # MyTrysAll
+主页：http://localhost:8080/MyTrysAll/
+      http://localhost:8080/MyTrysAll/ex
+统计/通知/报名 功能页
+
+
+
 SpringMVC4.0，HTML5，Thymeleaf，Mybatis，MySQL，AngularJS，Jquery，Redis，Spark
 
 apache-tomcat-8.0.18
@@ -15,4 +21,6 @@ redis-commons-pool2.4.2
 Jquery1.12.1
 
 AngularJS2.0.0
+
+jquery-easyui-1.5.1
 
--- src/main/java/mine/mytrysall/ControllerMainIndex.java
+++ src/main/java/mine/mytrysall/ControllerMainIndex.java
@@ -1,9 +1,13 @@
 package mine.mytrysall;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/*")
@@ -30,6 +34,43 @@ public class ControllerMainIndex {
     public String index(Model model) {
         model.addAttribute("recipient", "World");
         return "index";
+    }
+
+    @RequestMapping(value = "/teachtools", method = RequestMethod.GET)
+    public String teachtools(Model model) {
+        // model.addAttribute("recipient", "World");
+        return "teachtools";
+    }
+
+    @RequestMapping(value = "json/datagrid_data1.json", method = RequestMethod.GET)
+    @ResponseBody
+    public Object datagrid(Model model) {
+        // model.addAttribute("recipient", "World");
+
+        // Map<String, Object> result = new HashMap<String, Object>();
+        // List<User> users = userService.getAllUser(null, null);
+        // JSONArray jsonArray = new JSONArray();
+        // for (User user : users) {
+        // JSONObject jsonObject = new JSONObject();
+        // jsonObject.put("id", user.getUid());
+        // jsonObject.put("name", user.getName());
+        // jsonObject.put("age", user.getAge());
+        // jsonObject.put("phone", user.getPhone());
+        // jsonArray.add(jsonObject);
+        // }
+        // result.put("total", users.size());
+        // result.put("rows", jsonArray);
+        // return JSONObject.fromObject(result);
+
+        Map<String, String> result = new HashMap<String, String>();
+        result.put("productid", "FI-SW-01");
+        result.put("unitcost", "10.00");
+        result.put("status", "P");
+        result.put("listprice", "36.50");
+
+        // JSONObject jsonObject = new JSONObject();
+        // jsonObject = JSONObject.fromObject(result);
+        return result;
     }
 
     // @RequestMapping(value = "/test", method = RequestMethod.GET)
